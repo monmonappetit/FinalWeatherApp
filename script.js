ac9064eaba22259e67b5b7e72b19da3be32718df
monmonappetit <98507227+monmonappetit@users.noreply.github.com>
2022-02-07 20:43:34 -0800

    Updated humidity and windspeed innerHTML messages, and removed  font size from temperature

--- script.js
+++ script.js
@@ -59,8 +59,8 @@ function displayTemperature(response) {
     let yourWindspeed = document.querySelector("#windSpeed");
     let yourWeather = document.querySelector("#temperature");
     yourCurrentWeather.innerHTML = "🌤 " + response.data.weather[0].description;
-    yourHumidity.innerHTML = "🔥 " + response.data.main.humidity;
-    yourWindspeed.innerHTML = "🍃 " + Math.round(response.data.wind.speed);
+    yourHumidity.innerHTML = "🔥 Humidity: " + response.data.main.humidity;
+    yourWindspeed.innerHTML = "🍃 Windspeed: " + Math.round(response.data.wind.speed);
     yourWeather.innerHTML = `🌡 ${yourTemperature}° F`;
 }
 
